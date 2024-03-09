cf59089ca3ec98152e0f79d3317b55cfbb4efa8e
Masve <svendsenorway@hotmail.com>
2024-03-09 22:29:06 +0100

    changes to both herosections in index and whysnowcell

--- src/components/react/material-tailwind/HeroSectionAccordion.jsx
+++ src/components/react/material-tailwind/HeroSectionAccordion.jsx
@@ -3,9 +3,8 @@ import React, { useState, useEffect } from 'react';
  
 export default function HeroSectionAccordion() {
   const [module, setModule] = useState(null);
-  const [alwaysOpen, setAlwaysOpen] = useState(true);
 
-
+  
   useEffect(() => {
     (async () => {
       const materialTailwind = await import('@material-tailwind/react');
@@ -18,7 +17,7 @@ export default function HeroSectionAccordion() {
   // const handleOpenAcc1 = () => setOpenAcc1((cur) => !cur);
 
   // Currently in use: always open
-  // const [alwaysOpen, setAlwaysOpen] = useState(true);
+  const [alwaysOpen, setAlwaysOpen] = useState(true);
   const handleAlwaysOpen = () => setAlwaysOpen((cur) => !cur);
 
 
@@ -31,7 +30,7 @@ export default function HeroSectionAccordion() {
   return (
     <>
       <Accordion open={alwaysOpen} className="dark:text-white">
-        <AccordionHeader onClick={handleAlwaysOpen}>Why SnowCell?</AccordionHeader>
+        <AccordionHeader>Why SnowCell?</AccordionHeader>
         <AccordionBody className="dark:text-gray-100">
           We are faster, cheaper, and more efficient than traditional legacy providers. 
           Gone are the days of complex setups, slow deployments, and escalating costs. 
